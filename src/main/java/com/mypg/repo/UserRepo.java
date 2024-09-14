be99ca1b6349d40f6bf1e959d93aa56676ab3048
Vivek Kumar <vivekk12112@gmail.com>
2024-09-15 00:40:45 +0530

    half done signup page css left

--- src/main/java/com/mypg/repo/UserRepo.java
+++ src/main/java/com/mypg/repo/UserRepo.java
@@ -1,6 +1,7 @@
 package com.mypg.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.mypg.models.User;
 
 public interface UserRepo extends JpaRepository<User,Long> {
 
