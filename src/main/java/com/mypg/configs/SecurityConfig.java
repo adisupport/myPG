7bf97247142fe1090cd129de3effadba201f71cc
adi <aditechhelp@gmail.com>
2024-09-22 01:36:27 +0530

    Added 2 feature one owner credential set from application.properties file. 2.Handle NoResouceFoundException fallback to root controller to redirect

--- src/main/java/com/mypg/configs/SecurityConfig.java
+++ src/main/java/com/mypg/configs/SecurityConfig.java
@@ -2,36 +2,35 @@ package com.mypg.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.util.Collection;
-import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    Environment env;
+    SecurityConfig(Environment environment) {
+        this.env = environment;
+    }
 
     @Bean
     public SecurityFilterChain config(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
-            authorizationManagerRequestMatcherRegistry
-                .requestMatchers("/guest/**").hasRole("GUEST")
-                .requestMatchers("/admin/**").hasRole("ADMIN")
-                .requestMatchers("/owner/**").hasRole("OWNER")
-                .requestMatchers("/register/**","/login/**").permitAll()
-                .anyRequest().authenticated();
-        }).formLogin(withDefaults())
+        http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
+            .requestMatchers("/guest/**").hasRole("GUEST")
+            .requestMatchers("/admin/**").hasRole("ADMIN")
+            .requestMatchers("/owner/**").hasRole("OWNER")
+            .requestMatchers("/register/**","/login/**").permitAll()
+            .anyRequest().authenticated()).formLogin(withDefaults())
                 .logout(withDefaults());
         return http.build();
     }
@@ -43,9 +42,24 @@ public class SecurityConfig {
 
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails admin = User.builder().username("admin").password(passwordEncoder().encode("admin@123")).roles("ADMIN").build();
-        UserDetails owner = User.builder().username("owner").password(passwordEncoder().encode("owner@123")).roles("OWNER").build();
-        UserDetails guest = User.builder().username("guest").password(passwordEncoder().encode("guest@123")).roles("GUEST").build();
+
+        String owner_username=env.getProperty("owner.username");
+        String owner_password=env.getProperty("owner.password");
+        String admin_username=env.getProperty("admin.username");
+        String admin_password=env.getProperty("admin.password");
+
+        assert owner_username != null;
+        assert owner_password != null;
+
+        assert admin_username != null;
+        assert admin_password != null;
+
+        UserDetails admin = User.builder().username(admin_username)
+                .password(passwordEncoder().encode(admin_password)).roles("ADMIN").build();
+        UserDetails owner = User.builder().username(owner_username)
+                .password(passwordEncoder().encode(owner_password)).roles("OWNER").build();
+        UserDetails guest = User.builder().username("guest")
+                .password(passwordEncoder().encode("guest@123")).roles("GUEST").build();
         return new InMemoryUserDetailsManager(admin,guest,owner);
     }
 }
